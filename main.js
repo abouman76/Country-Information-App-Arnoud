5da4ac2a19885e0e631158f6adeb979afaafa03d
abouman76 <70742333+abouman76@users.noreply.github.com>
2021-02-12 09:47:26 +0100

    working reduce function and some styling

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ async function countryData() {
         //     "https://restcountries.eu/rest/v2/name/belgium"
         // );
         const responseCountry = await axios.get (
-            "https://restcountries.eu/rest/v2/name/tuvalu?fullText=true"
+            "https://restcountries.eu/rest/v2/name/tuvalu?fullText=true" //tuvalu
         );
 
         // console.log(responseCountry);
@@ -29,67 +29,60 @@ async function countryData() {
 }
 countryData();
 
-// 1. *************
+// VRAAG 1. *************
 const searchButton = document.getElementById("search-button");
 searchButton.addEventListener("click", countryData);
 
-//2. ***************
+// VRAAG 2. ***************
 // Maak een string die het volgende logt:
 // `[country-naam] is situated in [subarea-name]. It has a population of [amount] people.`
 // const displayCountryInfo = `${infoBelgium.name}`;
 // console.log(displayCountryInfo);
 
-        function currenciesCountry(currencies) {
-            // console.log("Valuta?", currencies);
-            let allCurrencies = "";
-            for (let i = 0; i < currencies.length ; i++) {
-                const currency = currencies[i];
-                // console.log("INDEX", currency);
-                if (currencies.length === 1) {
-                    // console.log("CURRENCY?", currency.name);
-                    const oneValuta = currency.name;
-                    // console.log("In EURO?", oneValuta);
-                    const resultValuta = `and you can pay with ${oneValuta}'s`;
-                    allCurrencies += oneValuta;
-                    console.log(resultValuta);
-                }
-                // [] 2 valuta's => you can pay with {}'s and {}'s
-                else if(currencies.length === 2) {
-                    const twoValutas = currency.name;
-                    const resultTwoValutas = `and you can pay with ${currency.name}'s and ${currency.name}'s`;
-                    allCurrencies += resultTwoValutas;
-                    console.log(resultTwoValutas);
-                }
+// VRAAG 4
 
-            }
+// function currenciesCountry(currencies) {
+//     // console.log("Valuta?", currencies);
+//     let allCurrencies = "";
+//     for (let i = 0; i < currencies.length ; i++) {
+//         const currency = currencies[i];
+//         // console.log("INDEX", currency);
+//         if (currencies.length === 1) {
+//             // console.log("CURRENCY?", currency.name);
+//             const oneValuta = currency.name;
+//             // console.log("In EURO?", oneValuta);
+//             const resultValuta = `and you can pay with ${oneValuta}'s`;
+//             allCurrencies += oneValuta;
+//             console.log("One valuta?", resultValuta);
+//         }
+        //[] 2 valuta's => you can pay with {}'s and {}'s
+        // else (currencies.length === 2) {
+        //     const twoValutas = currency.name;
+        //     const resultTwoValutas = `and you can pay with ${currency.name}'s and ${currency.name}'s`;
+        //     // const resultTwoValutas = `${resultValuta} and ${twoValutas}'s`;
+        //     allCurrencies += resultTwoValutas;
+        //     console.log(resultTwoValutas);
+        // }
+   // }
 
-            return allCurrencies;
+   // return allCurrencies;
 
-
-            // const formattedSizeWithPike = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm) |`;
-            // console.log(formattedSizeWithPike);
-            // allSizes = allSizes + formattedSizeWithPike;
-            // console.log(allSizes);
-
-            // const lastEntrySize = screenSize.availableSizes.length -1;
-            // console.log("Gelijk aan:" , lastEntrySize, screenSize.availableSizes.length -1 === i);
-
-            //if(screenSize.availableSizes.length -1 === i) {
-            // console.log("Last:" )
-            // const formattedSize = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm)`;
-            // console.log(formattedSize);
-            //  allSizes = allSizes + formattedSize;
-
-            // } else {
-            // console.log("Pike toevoegen?");
-            //  const formattedSizeWithPike = `${currentScreenSize} inches (${Math.round(currentScreenSize * 2.54)} cm) | `;
-            // console.log(formattedSizeWithPike);
-            //  allSizes = allSizes + formattedSizeWithPike;
-            //  }
-            //   }
-            // return allSizes;
+   // console.log(countryCurrency);
 
 
-// console.log(screenSizesTV(inventory[1]));
-        }
+function currenciesCountry(currencies) {
+    console.log("Functie aangeroepen?", currencies);
+    const allCurrencies = currencies.reduce((acc, currency, index) => {
+        console.log("Laatste valuta", currency, index === currencies.length -1);
 
+        if(index !== currencies.length -1 || currencies.length === 1){
+        return acc + `${currency.name}'s `;
+    }
+        if(index === currencies.length -1) {
+        return acc + `and ${currency.name}'s `;
+    }
+
+},"and you can pay with ");
+    return allCurrencies;
+}
+// console.log(currenciesCountry([{name: "euro"}, {name: "dollar"}]));
